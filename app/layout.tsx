cb7e93d7ba4f89a11eb31f1fa0add36d6931d291
hmgn <hmgrenner@gmv.com>
2023-10-15 19:45:08 +0200

    Updating images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,35 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Hex Realm',
-  description: 'Hex Realm Game - Coming soon',
-}
+let description = "Hex Realm - Coming soonn";
+let ogimage = "https://www.hexrealm.com/og.png";
+let sitename = "hexrealm.com";
+let title = "Hex Realm";
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<meta name="description" content={description!} />
+				<meta property="og:site_name" content={sitename} />
+				<meta property="og:description" content={description} />
+				<meta property="og:title" content={title} />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content={title} />
+				<meta name="twitter:description" content={description} />
+				<meta property="og:image" content={ogimage} />
+				<meta name="twitter:image" content={ogimage} />
+			</Head>
+			<body className={inter.className}>{children}</body>
+		</html>
+	)
 }
